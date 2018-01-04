d41bc480161bb2c4a9a066232e5642fbe976004e
Miguel Reboiro-Jato <mrjato@uvigo.es>
2018-01-04 18:08:17 +0100

    Adds license headers and updates dependencies
    License headers have been added to source files.
    
    In addition, dependencies version has been updated and some unused dependencies
    and files have been removed.

--- src/utils/objectByString.js
+++ src/utils/objectByString.js
@@ -1,3 +1,23 @@
+/*
+ * MultiTasking Cubes Administration
+ * Copyright (C) 2017-2018 - Miguel Reboiro-Jato, Francisco Rojas Rodríguez,
+ * Adolfo Piñón Blanco, Hugo López-Fernández, Rosalía Laza Fidalgo,
+ * Reyes Pavón Rial, Francisco Otero Lamas, Adrián Varela Pomar,
+ * Carlos Spuch Calvar, and Tania Rivera Baltanás
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 export default function (o, s) {
   s = s.replace(/\[(\w+)\]/g, ".$1"); // convert indexes to properties
   s = s.replace(/^\./, "");           // strip a leading dot
